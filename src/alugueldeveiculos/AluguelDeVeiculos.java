ae104bfbc315b1c7a51180a61a7114aef814a74d
IgMiras <im.miras@unesp.br>
2023-09-13 12:19:03 -0300

    corrigindo bugs

---
+++ src/alugueldeveiculos/AluguelDeVeiculos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alugueldeveiculos;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.time.Period;
+
+/**
+ *
+ * @author Windows
+ */
+public class AluguelDeVeiculos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LocalDate from = LocalDate.of(2023, 10, 9);
+        LocalDate to = LocalDate.of(2023, 10, 10);
+
+        long daysDifference = Math.abs(from.toEpochDay() - to.toEpochDay());
+
+        System.out.println("Difference in days: " + daysDifference);
+    }
+    
+}
---
+++ src/control/Controlador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+
+import models.LocadoraDeVeiculos;
+/**
+ *
+ * @author Windows
+ */
+
+public class Controlador {
+    private LocadoraDeVeiculos locadora = LocadoraDeVeiculos.getInstancia("AluguelDeVeiculos"
+            , "R. Osvaldo Peretti, 98. Jardim das Rosas. Presidente Prudente - SP"
+            , "www.alugueldeveiculos.com.br"
+            ,"Instagram: @AluguelDeVeiculos");
+}
