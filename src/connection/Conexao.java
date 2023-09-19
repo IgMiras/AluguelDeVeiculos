3a46bd51830fb4338d618122353672f77eba5d95
Igor Miras <im.miras@unesp.br>
2023-09-18 21:49:42 -0300

    MySQL, JDBC e DAO adicionados

---
+++ src/connection/Conexao.java
@@ -0,0 +1,26 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String url = "jdbc:mysql://localhost:3306/alugueldeveiculos";
+    private static final String user = "root";
+    private static final String password = "root";
+    private static Connection conexao;
+    
+    public static Connection getConexao(){
+        try {
+            if (conexao == null){
+                conexao = DriverManager.getConnection(url, user, password);
+                return conexao;
+            } else {
+                return conexao;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    } 
+}
